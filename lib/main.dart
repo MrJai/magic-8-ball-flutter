f395e7251d47d671680b87d8fcf715f82477f21e
Junaid Rehmat <junaid.rehmat.rana@gmail.com>
2020-05-12 21:22:35 +0500

    Add image in the center and randomly update image on the button tap.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Magic8Ball(),
+    ),
+  ),
+);
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+
+  int ballNumber = 1;
+
+  void randomizeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+              children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                randomizeBallNumber();
+              },
+              child: Image.asset(
+                'images/ball$ballNumber.png'
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
